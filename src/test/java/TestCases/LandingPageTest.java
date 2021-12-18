1e44a03bf4a542afe96fb3d4df456187b589402a
sambitD007 <sambitdash.2011@gmail.com>
2021-12-19 00:52:09 +0530

    Driver and logger classes added

--- src/test/java/TestCases/LandingPageTest.java
+++ src/test/java/TestCases/LandingPageTest.java
@@ -1,13 +1,24 @@
 package TestCases;
 
 import BaseClasses.BaseDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LandingPageTest {
 
-    @Test
-    public void findData() throws Exception {
+    @BeforeTest
+    public void setDriver() throws Exception {
         BaseDriver.setDriver();
+    }
+
+    @Test
+    public void findData(){
+    }
+
+    @AfterTest
+    public void closeDriver(){
         BaseDriver.closeDriver();
     }
 }
